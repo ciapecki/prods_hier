a18e6dfc767b35891632f254848abdfeb626b580
ciapecki <chris@goxp.de>
2020-10-26 10:39:32 +0100

    toolbar button macro jumpFreeze(*args) needed in LO: Tools->Customize->Toolbars->Category(Macros)->findYourMacroFunction->rightArrow on the wanted possition

--- jump_and_freeze.py
+++ jump_and_freeze.py
@@ -18,33 +18,20 @@ def call_dispatch(doc, url, args=()):
     dispatch.executeDispatch(frame, url, '', 0, args)
     return
 
+
+def jumpFreeze(*args):
+
+    args = ['']
+    args[0] = PropertyValue()                 # Default constructor
+    args[0].Name = "Nr"
+    args[0].Value = 2
+
+    call_dispatch(doc, '.uno:JumpToTable',args)
+    call_dispatch(doc, '.uno:FreezePanesFirstRow')
+
+
 CTX = uno.getComponentContext()
 SM = CTX.getServiceManager()
 doc = XSCRIPTCONTEXT.getDocument()
 
-args = ['']
-args[0] = PropertyValue()                 # Default constructor
-args[0].Name = "Nr"
-args[0].Value = 2
-
-call_dispatch(doc, '.uno:JumpToTable',args)
-call_dispatch(doc, '.uno:FreezePanesFirstRow')
-
-# FROM BASIC Macro Record
-#sub freeze1row
-#dim document   as object
-#dim dispatcher as object
-#
-#rem get access to the document
-#document   = ThisComponent.CurrentController.Frame
-#dispatcher = createUnoService("com.sun.star.frame.DispatchHelper")
-#
-#dim args1(0) as new com.sun.star.beans.PropertyValue
-#args1(0).Name = "Nr"
-#args1(0).Value = 2
-#
-#dispatcher.executeDispatch(document, ".uno:JumpToTable", "", 0, args1())
-#
-#dispatcher.executeDispatch(document, ".uno:FreezePanesFirstRow", "", 0, Array())
-#
-#end sub
+jumpFreeze()
